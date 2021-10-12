0459d2c0b23927c0fe61c05bfbc613d0e5566627
Derrkar <grod.j.java@gmail.com>
2021-10-12 12:51:38 +0300

    third commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,8 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static')
+]
+
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
